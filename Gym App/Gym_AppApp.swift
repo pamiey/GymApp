c5a77e20a966e83fcf639d8d20d42d4dd47307be
Michelle <pemioyetade@gmail.com>
2023-07-17 15:09:31 -0400

    Initial Commit

---
+++ Gym App/Gym_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_AppApp.swift
+//  Gym App
+//
+//  Created by Oluwapemi Oyetade on 2023-07-17.
+//
+
+import SwiftUI
+
+@main
+struct Gym_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
